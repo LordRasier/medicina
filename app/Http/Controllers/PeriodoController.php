2f1224f49f1a47272f20ba68fee116626d2cafa0
kellian <kellian.rea@groobyte.com>
2019-11-05 15:00:04 -0300

    update

--- app/Http/Controllers/PeriodoController.php
+++ app/Http/Controllers/PeriodoController.php
@@ -1341,7 +1341,7 @@ class PeriodoController extends Controller
             }
 
         }
-
+        dd($marcar);
 
         return view("periodo.estadoanual",[
             "periodo" => $periodo,
