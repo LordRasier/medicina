1066be7ce10bf7cf0d4aa3fbcdea000671e75bd9
kellian <kellian.rea@groobyte.com>
2019-11-05 14:38:39 -0300

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -171,6 +171,7 @@ class ProfileController extends Controller
         $user->type = $data["tipo"];
         $user->specialty = $data["especialidad"];
         $user->code = $data["codigo"];
+        dd(Carbon::parse($data["ingreso"])->format("Y-m-d"));
         $user->ingreso = Carbon::parse($data["ingreso"])->format("Y-m-d");
 
         $menus = sub_menu::all();
